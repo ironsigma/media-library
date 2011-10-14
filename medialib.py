4cb4ee99a926d5c8f9cdc30223ec280fbf100f6b
Juan D Frias <juandfrias@gmail.com>
2011-10-13 21:27:34 -0700

    Simplify grid build

--- medialib.py
+++ medialib.py
@@ -15,14 +15,16 @@ from medialib.model import Media
 
 class MediaLibrary(QWidget):
     MOVIES = '/data/Media/Movies/Children/'
-    COLS = 7
     THUMB_WIDTH = 143
-    THUMB_HEIGHT = 200 # THUMB_WIDTH * ASPECT_RATIO(1.4)
+    THUMB_HEIGHT = 200
 
     def __init__(self, parent=None):
         super(self.__class__, self).__init__(parent)
+        self.setGeometry(100, 50, 1400, 800)
+        self.setWindowTitle('Media Library')
 
         Cover.COVER_PATH = '/data/Media/Movies/Children/Covers/'
+        self.cover_list = []
 
         #ui
         self.grid = QGridLayout()
@@ -37,17 +39,16 @@ class MediaLibrary(QWidget):
         session = Session()
         media_service = MediaService(session)
 
-        # movie table
-        media_list = media_service.find()
-        rows = int(math.ceil(media_list.count() / float(MediaLibrary.COLS)))
-        row = 0
-        col = 0
-        for media in media_list:
-            if col >= MediaLibrary.COLS:
-                col = 0
-                row += 1
-            self._add_movie(row, col, media)
-            col += 1
+        # movies
+        for media in media_service.find():
+            cover = Cover(self, media, MediaLibrary.THUMB_WIDTH, MediaLibrary.THUMB_HEIGHT)
+            cover.clicked.connect(self._start_movie)
+            self.cover_list.append(cover)
+
+        # build grid
+        columns = self.width() // (MediaLibrary.THUMB_WIDTH + 10)
+        for (cover, row, col) in self._get_grid_pos(self.cover_list, columns):
+            self.grid.addWidget(cover, row, col)
 
         # add scroll area
         scrollArea = QScrollArea()
@@ -61,10 +62,17 @@ class MediaLibrary(QWidget):
         media = self.sender().media
         subprocess.Popen(['/usr/bin/totem', '--replace', MediaLibrary.MOVIES + media.file])
 
-    def _add_movie(self, row, col, media):
-        cover = Cover(self, media, MediaLibrary.THUMB_WIDTH, MediaLibrary.THUMB_HEIGHT)
-        self.grid.addWidget(cover, row, col)
-        cover.clicked.connect(self._start_movie)
+    def _get_grid_pos(self, items, columns):
+        pos = []
+        row = 0
+        col = 0
+        for item in items:
+            if col >= columns:
+                col = 0
+                row += 1
+            pos.append((item, row, col))
+            col += 1
+        return pos
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
